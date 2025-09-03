40a2992d6c3fbaa3d5e14c3c25f27c345ade2f91
IcedMilkOG <78520086+IcedMilkOG@users.noreply.github.com>
2025-09-03 17:54:13 -0400

    Update tttttt.lua

--- tttttt.lua
+++ tttttt.lua
@@ -146,7 +146,7 @@ function library.new(name)
         KeyLabel.Position = UDim2.new(0.5, -100, 0.2, 0)
         KeyLabel.Size = UDim2.new(0, 200, 0, 36)
         KeyLabel.Font = TextFont
-        KeyLabel.Text = "\t Keltec\n\n\nROST\n\n"
+        KeyLabel.Text = "Keltec ROST\n\n"
         KeyLabel.TextColor3 = TextColor
         KeyLabel.TextSize = 25
         KeyLabel.TextXAlignment = Enum.TextXAlignment.Center
